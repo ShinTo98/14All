c9a532e105b7be932e6816288313df6d0e9936d1
Shantao Yi <shantao.yi@gmail.com>
2017-10-22 03:20:09 -0700

    Front End Ver. 1.1.1: Demo Ready w/ T & N Functions

--- main.js
+++ main.js
@@ -5,3 +5,21 @@ $(document).ready(function() {
 		$("#buy_page").fadeIn(1200);
 	});
 });
+
+$(document).ready(function() {
+	$("#nope_button").click(function() {
+		$("#second_page").animate({bottom: '0px'}, 1000);
+		$("#first_page").animate({top: '800px'}, 500);
+		$("#first_page").fadeOut('slow');
+		$("#second_page").fadeIn(1200);
+	});
+});
+
+$(document).ready(function() {
+	$("#rethink_button").click(function() {
+		$("#third_page").animate({bottom: '0px'}, 1000);
+		$("#second_page").animate({right: '1100px'}, 500);
+		$("#second_page").fadeOut('slow');
+		$("#third_page").fadeIn(1200);
+	});
+});
